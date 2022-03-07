c0350400a04b80dee4c55a1d70ab6ec0ef47fc3c
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-03-07 17:58:53 +0800

    Add Lecture 1 Assignment

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Declare the variables.
+    int minuend, subtrahend, difference;
+    // Assign values to the variables.
+    minuend = 87;
+    subtrahend = 15;
+    // Subtract and save the difference.
+    difference = minuend - subtrahend;
+    // Display the result on the terminal.
+    printf("The result of %d minus %d is %d.\n", minuend, subtrahend, difference);
+    return 0;
+}
